533c62f16ae908572e16b6dbb4dc36cc2f471e3f
matthewcuan <cuanmatthewl@gmail.com>
2023-03-16 22:25:40 -0700

    updated constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,6 @@
-export const BASE_API_URL = 'http://localhost:8000/api/v1/users/';
-export const GAMES_API_URL = 'http://localhost:8000/api/v1/games/';
-export const HOST_URL = 'http://localhost:3000/'
+export const BASE_API_URL = 'https://chesswithfriends-server.herokuapp.com/api/v1/users/';
+// export const BASE_API_URL = 'http://localhost:8000/api/v1/users/';
+export const GAMES_API_URL = 'https://chesswithfriends-server.herokuapp.com/api/v1/games/';
+// export const GAMES_API_URL = 'http://localhost:8000/api/v1/games/';
+export const HOST_URL = 'https://melodious-speculoos-b36439.netlify.app/'
+// export const HOST_URL = 'http://localhost:3000/'
