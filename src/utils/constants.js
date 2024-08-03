b0ba1ee4c10bef2250e57dacfaee052dbbc51b74
matthewcuan <cuanmatthewl@gmail.com>
2024-08-03 10:33:59 -0700

    update constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,12 @@
-export const BASE_API_URL = 'https://chesswithfriends-server.herokuapp.com/api/v1/users/';
-export const GAMES_API_URL = 'https://chesswithfriends-server.herokuapp.com/api/v1/games/';
+export const BASE_API_URL = 'https://chess-backend-xi.vercel.app/api/v1/users/';
+export const GAMES_API_URL = 'https://chess-backend-xi.vercel.app/api/v1/games/';
 export const SOCKET_URL = 'https://chesswithfriends-socketio.herokuapp.com'
 export const HOST_URL = 'https://melodious-speculoos-b36439.netlify.app/'
+
+// export const BASE_API_URL = 'https://chesswithfriends-server.herokuapp.com/api/v1/users/';
+// export const GAMES_API_URL = 'https://chesswithfriends-server.herokuapp.com/api/v1/games/';
+// export const SOCKET_URL = 'https://chesswithfriends-socketio.herokuapp.com'
+// export const HOST_URL = 'https://melodious-speculoos-b36439.netlify.app/'
 
 // for local testing
 // export const BASE_API_URL = 'http://localhost:8000/api/v1/users/';
